bce10126ab650bdba2c4aae1b18707b74e8f9538
KOUSIK <155892308+kousik1103@users.noreply.github.com>
2025-10-30 10:46:30 +0530

    Add files via upload

---
+++ gcp-cloudbuild-demo-kc18/src/test/java/com/gcp/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gcp.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1,2);
+	}
+
+}
